357051e48388b5e4d49ee103bf6e88991f92fd11
Van Damme <m.van.damme@accenture.com>
2017-04-15 22:31:14 +0200

    Add files

---
+++ GladysApp/app.js
@@ -0,0 +1,8 @@
+// This is your first fully working Tabris.js app. Feel free to alter as you please.
+// Changes are saved automatically and are immediately available on your device.
+
+// Create a push button and add it to the content view
+new tabris.TextView({
+  centerX: 0, centerY: 0,
+  text: "My First App 22"
+}).appendTo(tabris.ui.contentView);
